66ab82bc1ff5b86f90c784b58ce2be58bebf77f0
leonelfer99 <leo99.fernandes@gmail.com>
2022-03-09 21:51:25 -0300

    agregado de fecha

--- indice/models.py
+++ indice/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.forms import DateField
 
 # Create your models here.
 
-class familiar(models.Model):
+class familiares(models.Model):
 
     nombre=models.CharField(max_length=40)
     apellido=models.CharField(max_length=40)
     dni=models.IntegerField(max_length=8)
+    nacimiento=models.DateField()
--- indice/views.py
+++ indice/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
-from indice.models import familiar
+from indice.models import familiares
+import datetime
 import sqlite3
 
 # Create your views here.
@@ -14,9 +15,8 @@ def plantilla(self):
     template=loader.get_template("indice.html")
 
 
-    familiar=familiar(nombre="", apellido="",dni=0)
+    familiar=familiares(nombre="leonel", apellido="fernandes",dni=41799417,nacimiento=datetime.date(1999,4,6))
 
-    #jasdksdf
     lista=[familiar]
     
     diccionario_de_datos={
@@ -25,4 +25,5 @@ def plantilla(self):
 
     # context= Context(diccionario_de_datos)
     plantilla_preparada=template.render(diccionario_de_datos) #antes le pasa el context
+
     return HttpResponse(plantilla_preparada)
